f90130165386e066350258fc56d41ecd79e7100a
CibeleYamada <111684211+CibeleYamada@users.noreply.github.com>
2022-09-24 17:32:28 -0300

    Criado aula1.js

---
+++ Aula 4/aula1.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`A velocidade do seu carro é de ${vel} km/h.`)
+if(vel > 60) { //condição composta
+    console.log(`Você ultrapassou a velocidade permitida de 60 km/h`)
+} else {
+    console.log(`Dirija com cuidado.`)
+}
